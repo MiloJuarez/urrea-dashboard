58c71a53922348476d0fb7b6f8e45b9d035861e7
Milo Juarez <milojuarez.dev@gmail.com>
2023-09-08 12:03:28 -0600

    Add class to register .env variables

--- Db/Connection.php
+++ Db/Connection.php
@@ -6,16 +6,20 @@ use PDO;
 
 class Connection
 {
-    private string $host = 'localhost';
-    private string $username = 'root';
-    private string $password = '';
-    private string $database = 'urrea';
+    private string $host;
+    private string $username;
+    private string $password;
+    private string $database;
 
     private PDO $connection;
 
     public function __construct()
     {
         if (!isset($this->connection)) {
+            $this->host = str_replace("'", "", getenv('HOST'));
+            $this->username = str_replace("'", "", getenv('USERNAME'));
+            $this->password = str_replace("'", "", getenv('PASSWORD'));
+            $this->database = str_replace("'", "", getenv('DATABASE'));
             try {
                 $connection = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->username, $this->password);
                 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 // Include files
-include "Utils/Routing.php";
+include_once "Utils/Routing.php";
+include_once "Utils/DotEnv.php";
 
 // Use classes;
 use Utils\Routing;
+use Utils\DotEnv;
+
+(new DotEnv(__DIR__ . '/.env'))->load();
 
 $request = $_SERVER['REQUEST_URI'];
 Routing::run($request);
